276c3c86672bc8a0bd7b5cbe56730ecbd6e1efa0
Tanguy-Richard <lorenzitanguy@gmail.com>
2022-07-26 14:43:56 +0200

    Ajout de données de vacances

---
+++ R/jours_feries-data.R
@@ -0,0 +1,15 @@
+#' Jours fériés en france entre 2000 et 2025
+#'
+#'
+#' @docType data
+#'
+#' @usage data(jours_feries)
+#'
+#' @format Un vecteur de date au format lubridate (YYYY-MM-DD).
+#'
+#' @source \href{https://calendrier.api.gouv.fr/jours-feries/metropole.json}{API gouvernement}
+#'
+#' @examples
+#' data(jours_feries)
+#'
+"jours_feries"
---
+++ R/vacances-data.R
@@ -0,0 +1,15 @@
+#' Vacances scolaire de l'académie de Rennes entre 2017 t 2023
+#'
+#'
+#' @docType data
+#'
+#' @usage data(vacances)
+#'
+#' @format Un tableau à 4 colonnes: (nom des vacances/début/fin/interval lubridate)
+#'
+#' @source \href{https://data.education.gouv.fr/api/v2/catalog/datasets/fr-en-calendrier-scolaire/exports/json}{API gouvernement}
+#'
+#' @examples
+#' data(vacances)
+#'
+"vacances"
